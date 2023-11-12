d14a21aaac349a0b614e1af9032734aac816149d
Naiyer Kalim <7639533+naiyerkalim@users.noreply.github.com>
2023-11-12 16:18:47 +0530

    All files added

---
+++ notes.js
@@ -0,0 +1,43 @@
+//div is a block level element.
+//span is inline element.
+//Attribute in Html: it provides additional information about element.
+//in href target= _blank, is used to get external tab.
+
+//bold/italic/underline/strikethrough
+  //<b>bold</b>
+  //<i>italic</i>
+  //<u>Underline</u>
+  //<s>Strikethrough</s>
+
+//pre tag: use to write text as it is, no change. 
+
+//superscript/subscript: use to put square or base number in formulas or equations.
+
+//Character entry reference: used to write reverse or special characters.
+  //syntax option stands with & and ends with ;.
+
+//View page source: It allows you to see the raw HTML, CSS, and JavaScript code that makes up the webpage.
+//Inspect Element: "Inspect Element" is a browser feature that allows you to examine and modify the HTML and CSS of a webpage in real-time.
+//validator.org.in: by using this site, we can validate our html code.
+
+//Semantic tags: Semantic tags are HTML elements that carry meaning about the structure and content of a webpage.
+  //Examples of semantic tags include <header>, <footer>, <article>, <section>, <nav>, <aside>, <main>, <figure>, and <figcaption>. 
+
+//aside: The <aside> tag in HTML is a semantic element used to define content that is tangentially related to the content around it. It is typically used for content like sidebars, pull quotes, or related links. 
+
+//inline element: These elements don't start on a new line and only occupy the width that their content requires.
+//Examples include <span>, <a>, <strong>, <em>, and <img>.
+
+//block element: These elements start on a new line and stretch to fill the width of their container.
+//Examples include <div>, <p>, <h1> to <h6>, <ul>, <li>, <form>, and <header>.
+
+// Classes:
+// Classes are used to define a group of elements that share the same style or behavior.
+// They can be applied to multiple elements within a document.
+
+// IDs:
+// IDs are used to uniquely identify a single element on a page.
+// They should only be assigned to one element per HTML document because their purpose is to uniquely identify an element.
+
+//Action: action attribute in a <form> element specifies the URL or endpoint where the form data will be sent when the form is submitted.
+
